5063fd39ae215cb831f082a932d7cad8b678ac78
Glitch (peerjs-video) <none>
2022-03-19 18:25:22 +0000

    🏙🦂 Checkpoint ./server.js:43125880/17

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/views/connection.html");
 });
 
 // listen for requests :)
