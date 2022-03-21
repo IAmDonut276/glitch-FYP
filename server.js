e4d81442df92dd59ca05bbb15d3d48ba4ebb2fbe
Glitch (peerjs-video) <none>
2022-03-21 07:58:58 +0000

    🚴💓 Checkpoint ./package.json:43125880/1326 ./server.js:43125880/398

--- server.js
+++ server.js
@@ -14,17 +14,18 @@ const app = express();
 app.use(express.static("public"));
 
 // https://expressjs.com/en/starter/basic-routing.html
-// TODO: You better put back the index.html here, else people see nothing when they go to your app
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/register.html");
 });
 
-//TODO: To make /login sering the login.html, you need to add a new app.get here
 app.get("/login", (request, response) => {
   response.sendFile(__dirname + "/views/login.html");
 });
 
-//TODO: then do another one for register, you want want to learn a bit about GET and POST
+app.get("/register", (request, response) => {
+  response.sendFile(__dirname + "/views/register.html");
+});
+
 app.get("/connection", (request, response) => {
   response.sendFile(__dirname + "/views/connection.html");
 });
