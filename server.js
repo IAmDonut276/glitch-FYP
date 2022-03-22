3322f46e36fe58fb21fecefda875bab8379e3050
Glitch (fyp-webrtc-cctv) <none>
2022-03-22 18:51:02 +0000

    Log Out in Connection.html

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get("/connection", (request, response) => {
   response.sendFile(__dirname + "/views/connection.html");
 });
 
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
